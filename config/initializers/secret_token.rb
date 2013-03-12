cc37a345b5bf77e5fc17d0d8e33d53432a488c0e
Pradeep Ittycheria <pittycheria@gmail.com>
2013-03-12 13:34:02 -0500

    intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleSpp::Application.config.secret_token = '8e0df59eefa156e6011e98dd329a6ad9421cf5e2fa3620fef359a4aad7f8d1ed49b5d449e5cf1ea53c975751bf94f096632c094fea1cff414bfcc3e73f0bac93'
